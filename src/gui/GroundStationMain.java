848f5c27fedc9756c7e1ff488b1d7e24a42fb2c5
JF21 <Justinfu21@gmail.com>
2015-12-10 20:24:05 -0500

    initial commit

--- src/gui/GroundStationMain.java
+++ src/gui/GroundStationMain.java
@@ -3,7 +3,12 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
 import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.geom.Point2D;
@@ -26,6 +31,8 @@ import com.digi.xbee.api.listeners.IDataReceiveListener;
 import com.digi.xbee.api.models.XBee64BitAddress;
 import com.digi.xbee.api.models.XBeeMessage;
 
+import gnu.io.CommPortIdentifier;
+
 public class GroundStationMain extends JFrame implements IDataReceiveListener, WindowListener{
 
 	//Debug boolean
@@ -35,7 +42,7 @@ public class GroundStationMain extends JFrame implements IDataReceiveListener, W
 	private static final long serialVersionUID = -5652170290197609712L;
 	
 	//Information for initializing Xbees
-	private static final String COM_PORT = "COM5";  //PLACEHOLDER
+	private static final String COM_PORT = "/dev/tty.usbserial-DA01OPLP";  //PLACEHOLDER
 	private static final int BAUD_RATE = 9600; //PLACEHOLDER
 	private static final String TRANSMITTER_ADDRESS = "0013A20040E6D613";
 
@@ -50,6 +57,7 @@ public class GroundStationMain extends JFrame implements IDataReceiveListener, W
 	private static final int NUM_DROPPED = 2;
 
 	// Member objects for each of the panels
+	private JMenuBar menuBar;
 	private DataChart altChart;
 	private Instruments altitudeSpeed;
 	private DropStatusPane payloadDrop;
@@ -68,27 +76,75 @@ public class GroundStationMain extends JFrame implements IDataReceiveListener, W
 	/**
 	 * Default constructor.
 	 */
-	public GroundStationMain() {		
+	public GroundStationMain() {
+		startTime = System.nanoTime();
 		// Set the window to take maximize to fill the whole screen.
 		super.setExtendedState(super.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 		// Exit the program when you close the window.
 		super.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
+		while( portEnum.hasMoreElements()){
+			CommPortIdentifier portIdentifier = portEnum.nextElement();
+            System.out.println(portIdentifier.getName()  +  " - " +  getPortTypeName(portIdentifier.getPortType()) );
+        }        
+		
 		initGui();
 		System.out.println("Hi");
-    	while (!setUpXBee(false)) { //multiple times, no error msg
-			try {Thread.sleep(1000);}
-			catch (InterruptedException e) {}
-		}  
 		
-		startTime = System.nanoTime();
+		if(!DEBUG_WITHOUT_RADIO){
+	    	while (!setUpXBee(false)) { //multiple times, no error msg
+				try {Thread.sleep(1000);}
+				catch (InterruptedException e) {}
+			}  
+		}
+		
+		
 		
 		super.setVisible(true);
 	}
 
+	 String getPortTypeName( int portType )
+	    {
+	        switch ( portType )
+	        {
+	            case CommPortIdentifier.PORT_I2C:
+	                return "I2C";
+	            case CommPortIdentifier.PORT_PARALLEL:
+	                return "Parallel";
+	            case CommPortIdentifier.PORT_RAW:
+	                return "Raw";
+	            case CommPortIdentifier.PORT_RS485:
+	                return "RS485";
+	            case CommPortIdentifier.PORT_SERIAL:
+	                return "Serial";
+	            default:
+	                return "unknown type";
+	        }
+	    }
+
 	private void initGui() {
 		setSize(300, 200);
 		setTitle("M-Fly Ground Station");
-		setUpXBee(true); //first time, print an error
+		
+		menuBar = new JMenuBar();
+		JMenu menu = new JMenu("Select COM Port");
+		menuBar.add(menu);
+		
+		JMenuItem menuItem = new JMenuItem("\u2713 A text-only menu item", KeyEvent.VK_T);
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(
+		        KeyEvent.VK_1, ActionEvent.ALT_MASK));
+		menuItem.getAccessibleContext().setAccessibleDescription(
+		        "This doesn't really do anything");
+		menu.add(menuItem);
+	    this.setJMenuBar(menuBar);
+		
+		
+		if(!DEBUG_WITHOUT_RADIO){
+			xbee = new XBeeDevice(COM_PORT, BAUD_RATE);
+			setUpXBee(true); //first time, print an error
+		}
+		
 		Container pane = getContentPane();
 		Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("HH-mm-ss");
@@ -100,25 +156,17 @@ public class GroundStationMain extends JFrame implements IDataReceiveListener, W
         out.print("Hello World");
         File file = new File(fileName);
         
-        if(!DEBUG_WITHOUT_RADIO){	
-			// Create a new XBee device object
-			xbee = new XBeeDevice(COM_PORT, BAUD_RATE);
-			
-			try {
-				xbee.open();
-				xbee.addDataListener(this);
-				System.out.println("setup success!");
-			} catch (XBeeException e) {
-				System.out.println("ERROR: THIS IS FOR ADAM THIS IS INFACT AN ERROR");
-				e.printStackTrace();
-			}
-		}
+
 		altitudeSpeed = new Instruments();
 		payloadDrop = new DropStatusPane();
 		
 		JComponent fpvCamera = new JButton("CAMERA");
 		//JComponent graph = new JButton("GRAPH");
-		altChart = new DataChart("Altitude", 3200,300);
+		int width = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		altChart = new DataChart("Altitude", width,300);
+
+		
+		
 		//graph.setPreferredSize(new Dimension(200, 300));
 		
 		pane.add(altitudeSpeed, BorderLayout.LINE_START);
@@ -129,15 +177,16 @@ public class GroundStationMain extends JFrame implements IDataReceiveListener, W
 		this.setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 		this.addWindowListener(this);
 		this.setVisible(true);
-        
+    
         System.out.println("Path : " + file.getAbsolutePath());
 		if(DEBUG_WITHOUT_RADIO){
 		// Simulate XBee messages to test that we interpret them correctly
 		this.testXBeeMessageParsing();
 		}
 	}
-
+    
 	private boolean setUpXBee(boolean firstTry) {
+		
 		if (xbee.isOpen()) return true;
 		try {
 			xbee.open();
