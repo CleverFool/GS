b70134ded9b63558f41b18623da28bfa778c85ea
DanChan3536 <dchandross@gmail.com>
2016-01-13 20:33:21 -0500

    Added Drop Points to graph
    and changed the color

--- src/gui/DataChart.java
+++ src/gui/DataChart.java
@@ -4,6 +4,7 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.Shape;
 import java.awt.geom.Point2D;
 import java.util.Random;
 
@@ -23,6 +24,7 @@ import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.util.ShapeUtilities;
 
 /*
  * Code Written by Daniel Chandross
@@ -51,8 +53,8 @@ public class DataChart extends JPanel
       dataPoints = new XYSeries( "Altitude" );   
       dropPoints = new XYSeries( "Dropped" );   
       dataSet = new XYSeriesCollection( );
-      dataSet.addSeries(dataPoints);
       dataSet.addSeries(dropPoints);
+      dataSet.addSeries(dataPoints);
       chart = ChartFactory.createXYLineChart(
          title, "Time (s)", "Altitude (m)", dataSet,
          PlotOrientation.VERTICAL ,true , true , false);
@@ -62,11 +64,14 @@ public class DataChart extends JPanel
       chartPanel.setPreferredSize(new Dimension(xSize, ySize));
       plot = chart.getXYPlot();
       renderer = new XYLineAndShapeRenderer( );
-      renderer.setSeriesPaint(0, Color.RED );
-      renderer.setSeriesStroke(0, new BasicStroke(4.0f));
       renderer.setSeriesPaint(1, Color.BLUE );
+      renderer.setSeriesStroke(0, new BasicStroke(4.0f));
+      renderer.setSeriesPaint(0, Color.YELLOW );
       renderer.setSeriesStroke(1, new BasicStroke(4.0f));
       plot.setRenderer( renderer ); 
+      renderer.setSeriesLinesVisible(0, false);
+      Shape cross = ShapeUtilities.createDiamond(7);
+      renderer.setSeriesShape(0, cross);
      ((XYPlot) chart.getPlot()).getDomainAxis().setUpperBound(30);
 
    }
@@ -83,10 +88,9 @@ public class DataChart extends JPanel
 	   
 	   dataPoints.add(inputPoint.getX(), inputPoint.getY());
 	   if (justDropped) {
-		      renderer.setSeriesPaint(0, Color.BLUE );
-		      dataPoints.add(inputPoint.getX(), inputPoint.getY());
-		      dataPoints.add(inputPoint.getX() + 1, inputPoint.getY());
-		      renderer.setSeriesPaint(0, Color.RED );
+		    //  renderer.setSeriesPaint(0, Color.BLUE );
+		      dropPoints.add(inputPoint.getX(), inputPoint.getY());
+		      //renderer.setSeriesPaint(0, Color.RED );
 	   } else {dataPoints.add(inputPoint.getX(), inputPoint.getY());}
    }
    
@@ -113,8 +117,9 @@ public class DataChart extends JPanel
     	 
     	  if (i >= 30) y = r.nextInt(4) + oldY - 2;
     	  //if it's the second half, set the rng to roughly make the points smaller on average
-    	  chart.update(new Point2D.Double(i,y)); //adds the point to the chart
-    	  try {Thread.sleep(1000);} catch (InterruptedException e) {} //sleep to simulate time
+    	  if (i % 10 == 0) chart.update(new Point2D.Double(i,y), true);
+    	  else chart.update(new Point2D.Double(i,y)); //adds the point to the chart
+    	  try {Thread.sleep(100);} catch (InterruptedException e) {} //sleep to simulate time
     	  oldY = y; //prepare for next loop
       }   
    }
