38296ee4e004662f2143187d44f14ff7d66a5f11
Ian O'Rourke <cessna.ian@gmail.com>
2016-02-03 20:21:34 -0500

    Cleaned up code

--- src/gui/GroundStationMain.java
+++ src/gui/GroundStationMain.java
@@ -3,42 +3,23 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Image;
-import java.awt.MenuBar;
-import java.awt.MenuItem;
-import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.awt.geom.Point2D;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Random;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
-import org.jfree.chart.plot.CategoryPlot;
-
-import com.digi.xbee.api.XBee;
 import com.digi.xbee.api.XBeeDevice;
 import com.digi.xbee.api.exceptions.XBeeException;
 import com.digi.xbee.api.listeners.IDataReceiveListener;
@@ -47,7 +28,7 @@ import com.digi.xbee.api.models.XBeeMessage;
 
 import gnu.io.CommPortIdentifier;
 
-public class GroundStationMain extends JFrame implements IDataReceiveListener, WindowListener, ActionListener{
+public class GroundStationMain extends JFrame implements IDataReceiveListener, ActionListener{
 
 	//Debug boolean
 	private static final boolean DEBUG_WITHOUT_RADIO = true;
@@ -92,7 +73,7 @@ public class GroundStationMain extends JFrame implements IDataReceiveListener, W
 
 	//MAIN
 	public static void main(String[] args) {
-		GroundStationMain gs = new GroundStationMain();
+		new GroundStationMain();
 	}
 
 	/**
@@ -109,12 +90,13 @@ public class GroundStationMain extends JFrame implements IDataReceiveListener, W
 		this.addWindowListener(new java.awt.event.WindowAdapter() {
 		    @Override
 		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-		    	
-		    	try{
-		    	xbee.close();
-		    	}catch(Exception e){}
-		       System.out.println("The Gui has been exited.");
-		    }
+	    	
+	    	try{
+	    		xbee.close();
+	    		out.close();
+	    	}catch(Exception e){}
+	    		System.out.println("The Gui has been exited.");
+	    	}
 		});
 		
 		detectComPorts();
@@ -271,7 +253,6 @@ String getPortTypeName( int portType )
 		pane.add(altChart, BorderLayout.PAGE_END);
 	
 		this.setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-		this.addWindowListener(this);
 		this.setVisible(true);
     
         System.out.println("Path : " + file.getAbsolutePath());
@@ -430,18 +411,5 @@ System.out.println(stringOutput);//+" "+stringOutput.substring(0, 1)+" "+stringO
 		    		comStatus.setForeground(Color.BLACK);
 		    	}
 	}
-	
-	//needed functions for WindowListener. Only here to close the writer when the X is clicked
-	@Override
-	public void windowActivated(WindowEvent arg0) {}
-	public void windowClosed(WindowEvent arg0) {}
-	public void windowClosing(WindowEvent arg0) {
-		out.close(); 
-		System.exit(0);
-		}
-	public void windowDeactivated(WindowEvent arg0) {}
-	public void windowDeiconified(WindowEvent e) {}
-	public void windowIconified(WindowEvent e) {}
-	public void windowOpened(WindowEvent e) {}
 }
 		
